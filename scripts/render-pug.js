24ec29d2b283ced406a06ed814cfed1bfb0fa76d
joonryul.lee <jrl1103@gmail.com>
2026-05-27 10:43:33 -0400

    Synced pug file with html

--- scripts/render-pug.js
+++ scripts/render-pug.js
@@ -6,20 +6,21 @@ const sh = require('shelljs');
 const prettier = require('prettier');
 
 module.exports = function renderPug(filePath) {
-    const destPath = filePath.replace(/src\/pug\//, 'dist/').replace(/\.pug$/, '.html');
+    const destPath = filePath.replace(/src[\/\\]pug[\/\\]/, 'dist/').replace(/\.pug$/, '.html');
+    const destPaths = [destPath];
     const srcPath = path.resolve(path.dirname(__filename), '../src');
+    const templatePath = path.resolve(path.dirname(__filename), '../templates/index.template.html');
 
-    console.log(`### INFO: Rendering ${filePath} to ${destPath}`);
+    if (path.basename(filePath) === 'index.pug') {
+        destPaths.push(templatePath);
+    }
+
+    console.log(`### INFO: Rendering ${filePath} to ${destPaths.join(', ')}`);
     const html = pug.renderFile(filePath, {
         doctype: 'html',
         filename: filePath,
         basedir: srcPath
     });
-
-    const destPathDirname = path.dirname(destPath);
-    if (!sh.test('-e', destPathDirname)) {
-        sh.mkdir('-p', destPathDirname);
-    }
 
     const prettified = prettier.format(html, {
         printWidth: 1000,
@@ -31,5 +32,12 @@ module.exports = function renderPug(filePath) {
         htmlWhitespaceSensitivity: 'ignore'
     });
 
-    fs.writeFileSync(destPath, prettified);
+    destPaths.forEach(destPath => {
+        const destPathDirname = path.dirname(destPath);
+        if (!sh.test('-e', destPathDirname)) {
+            sh.mkdir('-p', destPathDirname);
+        }
+
+        fs.writeFileSync(destPath, prettified);
+    });
 };
